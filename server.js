81d42a6eea416164da312e57ec12725ae7a361a1
Maxine Verdier <maxineverdier5@gmail.com>
2025-07-28 19:00:23 -0400

    port fix

--- server.js
+++ server.js
@@ -322,6 +322,6 @@ app.use((req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
