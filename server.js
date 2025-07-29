da0f5923ce57b8b721b4418684ab828ca7a0e804
Maxine Verdier <maxineverdier5@gmail.com>
2025-07-28 21:34:03 -0400

    fix health

--- server.js
+++ server.js
@@ -236,9 +236,6 @@ app.get('/qr-confirm/:qr_id', (req, res) => {
 app.use((req, res) => {
   res.status(404).send(`Route ${req.originalUrl} not found`);
 });
-app.get('/health', (req, res) => {
-  res.send('OK');
-});
 
 // Start the server
 const PORT = process.env.PORT || 10000;
